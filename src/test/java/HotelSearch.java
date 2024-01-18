b225e53fdda46ea460de24671e3f43321e253e67
bartoszlagoda <composit095@gmail.com>
2024-01-18 11:42:12 +0100

    SELENIUM-248: Iterating through hotel names

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -7,19 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
     @Test
     public void searchHotel() throws InterruptedException {
+
+        // Otworzenie przeglądarki ze stroną do testowania
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         // otworzenie okna przeglądarki na pełnym ekranie
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
+
+        // Wypełnienie pola 'Search by Hotel or City Name'
 
         WebElement searchByCityName = driver.findElement(By.className("select2-chosen"));
         searchByCityName.click();
@@ -34,10 +41,57 @@ public class HotelSearch {
         wait.until(ExpectedConditions.visibilityOfElementLocated(dubaiLocatorChoosen));
         driver.findElement(dubaiLocatorChoosen).click();
 
-        WebElement checkInOnMainPage = driver.findElement(By.name("checkin"));
-        checkInOnMainPage.sendKeys("20/01/2024");
+        // Wybranie daty przyjazdu i odjazdu
 
+        WebElement checkInOnMainPage = driver.findElement(By.name("checkin"));
+        checkInOnMainPage.sendKeys("20/01/2024"); // wpisanie daty
+        // wyklikanie daty wyjazdu
         WebElement checkOutOnMainPage = driver.findElement(By.name("checkout"));
-        checkOutOnMainPage.sendKeys("25/01/2024");
+        checkOutOnMainPage.click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='25']"))
+                        .stream()
+//                        .filter(el -> el.isDisplayed())
+                        .filter(WebElement::isDisplayed)
+                        .findFirst()
+//                        .ifPresent(el -> el.click());
+                        .ifPresent(WebElement::click);
+
+        // Wybranie liczby osób, które mają wyjechać
+        WebElement travellers = driver.findElement(By.name("travellers"));
+        travellers.click();
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.name("adults"))));
+        WebElement adults = driver.findElement(By.name("adults"));
+        adults.clear();
+        adults.sendKeys("2");
+        WebElement childsPlusBtn = driver.findElement(By.xpath("//button[@id='childPlusBtn']"));
+        childsPlusBtn.click();
+        childsPlusBtn.click();
+        WebElement child = driver.findElement(By.xpath("//input[@id='childInput']"));
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(adults.getAttribute("value"),"2");
+        softAssert.assertEquals(child.getAttribute("value"),"2");
+
+        // kliknięcie przycisku Search
+        WebElement searchBtn = driver.findElement(By.xpath("//div[@class='col-md-2 form-group go-right col-xs-12 search-button']/button[@type='submit']"));
+        searchBtn.click();
+
+        // przejście do strony filter search
+        List<String> hotelNames = driver.findElements(By.xpath("//h4[@class='RTL go-text-right mt0 mb4 list_title']//b")).stream()
+                .map(el -> el.getAttribute("textContent"))
+                .collect(Collectors.toList());
+
+        System.out.println(hotelNames.size());
+//        hotelNames.forEach(el -> System.out.println(el));
+        hotelNames.forEach(System.out::println); // dla każdego elementu hotelNames wykonaj sout
+
+        softAssert.assertEquals("Jumeirah Beach Hotel",hotelNames.get(0));
+        softAssert.assertEquals("Oasis Beach Tower",hotelNames.get(1));
+        softAssert.assertEquals("Rose Rayhaan Rotana",hotelNames.get(2));
+        softAssert.assertEquals("Hyatt Regency Perth",hotelNames.get(3));
+
+
+        driver.quit();
+        softAssert.assertAll();
     }
 }
