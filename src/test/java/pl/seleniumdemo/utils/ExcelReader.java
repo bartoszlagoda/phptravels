2ebc2f50fe5803e4abdf122d677f75023c2a7dfb
bartoszlagoda <composit095@gmail.com>
2024-01-24 13:27:43 +0100

    SELENIUM-283: Add comments

--- src/test/java/pl/seleniumdemo/utils/ExcelReader.java
+++ src/test/java/pl/seleniumdemo/utils/ExcelReader.java
@@ -13,24 +13,31 @@ import java.io.IOException;
 public class ExcelReader {
 
     public static void readExcelFile(String fileName) throws IOException {
+        // czytanie plików tylko znajdujących się w resources
         File file = new File("src/test/resources/" + fileName);
         FileInputStream inputStream = new FileInputStream(file);
 
+        // implementacja zależna od rozszerzenia pliku (dodane później)
         Workbook workbook = null;
+        // wyciąganie z dostarczonego stringa kropki i to co dalej (czyli rozszerzenie)
         String fileExt = fileName.substring(fileName.indexOf("."));
+        // wprowadzenie zależności dla implementacji workbooka
         if (fileExt.equals(".xlsx")) {
             workbook = new XSSFWorkbook(inputStream);
         } else if (fileExt.equals(".xls")) {
             workbook = new HSSFWorkbook(inputStream);
         }
-
+        // pobranie pierwszego arkusza z pliku excel
         Sheet sheet = workbook.getSheetAt(0);
-
+        // sprawdzenie ile wierszy mamy w pliku excel
         int rowCount = sheet.getLastRowNum();
 
+        // pomijamy pierwszy wiersz (z nagłówkami)
         for (int i = 1; i <= rowCount; i++) {
             Row row = sheet.getRow(i);
+            // pobranie wartości z pierwszej kolumny
             System.out.println(row.getCell(0).getStringCellValue());
+            // pobranie wartości z drugiej kolumny
             System.out.println(row.getCell(1).getStringCellValue());
         }
     }
