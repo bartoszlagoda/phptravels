2dce8bbe5e7e68f20e9f98ab67fc336ab572a85e
bartoszlagoda <composit095@gmail.com>
2024-01-25 12:23:37 +0100

    SELENIUM-286: Add reports and screenshot for SignUpTest

--- src/test/java/pl/seleniumdemo/tests/SignUpTest.java
+++ src/test/java/pl/seleniumdemo/tests/SignUpTest.java
@@ -1,5 +1,7 @@
 package pl.seleniumdemo.tests;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,14 +12,18 @@ import pl.seleniumdemo.model.User;
 import pl.seleniumdemo.pages.HotelSearchPage;
 import pl.seleniumdemo.pages.LoggedUserPage;
 import pl.seleniumdemo.pages.SignUpPage;
+import pl.seleniumdemo.utils.SeleniumHelper;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
 public class SignUpTest extends BaseTest {
 
     @Test
-    public void signUpHappyPathTest() {
+    public void signUpHappyPathTest() throws IOException {
+
+        ExtentTest test = extentReports.createTest("Sign up Happy Path Test");
 
         // Stworzenie zmiennych potrzebnych w dalszych krokach testu
         String lastname = "Lagoda";
@@ -27,16 +33,24 @@ public class SignUpTest extends BaseTest {
         // Klikanie na element 'My accounti i 'Sign Up'
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.openSignUpForm();
+        test.log(Status.PASS,"Opening sign up form", SeleniumHelper.getScreenshot(driver));
 
         // Wypełnianie pól do rejestracji
         SignUpPage signUpPage = new SignUpPage(driver);
         signUpPage.setFirstname("Bartosz");
+        test.log(Status.PASS,"Setting first name", SeleniumHelper.getScreenshot(driver));
         signUpPage.setLastname(lastname);
+        test.log(Status.PASS,"Setting last name", SeleniumHelper.getScreenshot(driver));
         signUpPage.setPhone("123456789");
+        test.log(Status.PASS,"Setting phone number", SeleniumHelper.getScreenshot(driver));
         signUpPage.setEmail(email);
+        test.log(Status.PASS,"Setting email", SeleniumHelper.getScreenshot(driver));
         signUpPage.setPassword("Password1234");
+        test.log(Status.PASS,"Setting password", SeleniumHelper.getScreenshot(driver));
         signUpPage.setConfirmPassword("Password1234");
+        test.log(Status.PASS,"Confirming password", SeleniumHelper.getScreenshot(driver));
         signUpPage.signUp();
+        test.log(Status.PASS,"Clicking Sign Up", SeleniumHelper.getScreenshot(driver));
         // sprawdzanie rezultatu logowania
         LoggedUserPage loggedUserPage = new LoggedUserPage(driver);
         wait.withTimeout(Duration.ofSeconds(1));
@@ -46,10 +60,13 @@ public class SignUpTest extends BaseTest {
         softAssert.assertTrue(loggedUserPage.getHeadingText().contains(lastname));
 
         softAssert.assertAll();
+        test.log(Status.PASS, "Assertions passed.", SeleniumHelper.getScreenshot(driver));
     }
 
     @Test
-    public void signUpHappyPathSecondTest() {
+    public void signUpHappyPathSecondTest() throws IOException {
+
+        ExtentTest test = extentReports.createTest("Sign up Happy Path Test [2]");
 
         // Stworzenie zmiennych potrzebnych w dalszych krokach testu
         String lastname = "Lagoda";
@@ -66,10 +83,12 @@ public class SignUpTest extends BaseTest {
         // Klikanie na element 'My accounti i 'Sign Up'
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.openSignUpForm();
+        test.log(Status.PASS,"Opening sign up form", SeleniumHelper.getScreenshot(driver));
 
         // Wypełnianie pól do rejestracji
         SignUpPage signUpPage = new SignUpPage(driver);
         signUpPage.fillSignUpForm(user);
+        test.log(Status.PASS,"Filling all registration fields properly", SeleniumHelper.getScreenshot(driver));
         // sprawdzanie rezultatu logowania
         LoggedUserPage loggedUserPage = new LoggedUserPage(driver);
         wait.withTimeout(Duration.ofSeconds(1));
@@ -79,14 +98,18 @@ public class SignUpTest extends BaseTest {
         softAssert.assertTrue(loggedUserPage.getHeadingText().contains(user.getLastName()));
 
         softAssert.assertAll();
+        test.log(Status.PASS, "Assertions passed.", SeleniumHelper.getScreenshot(driver));
     }
 
     @Test
-    public void signUpInvalidEmailTest() {
+    public void signUpInvalidEmailTest() throws IOException {
+
+        ExtentTest test = extentReports.createTest("Sign up Unhappy Path Test");
 
         // Klikanie na element 'My accounti i 'Sign Up'
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.openSignUpForm();
+        test.log(Status.PASS,"Opening sign up form", SeleniumHelper.getScreenshot(driver));
 
         String lastname = "Lagoda";
         int randomNumber = (int) (Math.random() * 1000);
@@ -95,6 +118,7 @@ public class SignUpTest extends BaseTest {
         // Wypełnianie pól do rejestracji
         SignUpPage signUpPage = new SignUpPage(driver);
         signUpPage.fillSignUpForm("Bartosz", lastname, "123456789", email, "Password1234", "Password1234");
+        test.log(Status.PASS,"Filling all registration fields", SeleniumHelper.getScreenshot(driver));
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='alert alert-danger']/p")));
 
@@ -105,14 +129,18 @@ public class SignUpTest extends BaseTest {
         softAssert.assertEquals(dangerAlertsAfterSignIn.get(0), "The Email field must contain a valid email address.");
 
         softAssert.assertAll();
+        test.log(Status.PASS, "Assertions passed.", SeleniumHelper.getScreenshot(driver));
     }
 
     @Test
-    public void signUpUnhappyPathTest() {
+    public void signUpUnhappyPathTest() throws IOException {
+
+        ExtentTest test = extentReports.createTest("Sign up Unhappy Path Test [2]");
 
         // Klikanie na element 'My accounti i 'Sign Up'
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.openSignUpForm();
+        test.log(Status.PASS,"Opening sign up form", SeleniumHelper.getScreenshot(driver));
 
         driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
         FluentWait<WebDriver> fluentWait = new FluentWait<>(driver);
@@ -131,6 +159,7 @@ public class SignUpTest extends BaseTest {
         softAssert.assertEquals(dangerAlertsAfterSignIn.get(4), "The Last Name field is required.");
 
         softAssert.assertAll();
+        test.log(Status.PASS, "Assertions passed.", SeleniumHelper.getScreenshot(driver));
 
     }
 }
